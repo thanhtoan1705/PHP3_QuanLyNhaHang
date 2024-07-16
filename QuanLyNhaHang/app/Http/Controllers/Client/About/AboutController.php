bea0822ead8e2a7e674842f4e0149190866e29f1
ngoc-cuong <94525355+Cuongnhn@users.noreply.github.com>
2024-07-16 18:23:43 +0700

    [Ngọc Cương] - sửa controller

--- QuanLyNhaHang/app/Http/Controllers/Client/About/AboutController.php
+++ QuanLyNhaHang/app/Http/Controllers/Client/About/AboutController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Client;
+namespace App\Http\Controllers\Client\About;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
--- QuanLyNhaHang/app/Http/Controllers/Client/Checkout/CheckoutController.php
+++ QuanLyNhaHang/app/Http/Controllers/Client/Checkout/CheckoutController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Client;
+namespace App\Http\Controllers\Client\Checkout;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
