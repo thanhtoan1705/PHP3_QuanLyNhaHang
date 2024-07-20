f7a9dd36fd0f1c35d7f0dc767e4370f76e363e6c
thanhtoan1705 <153188343+thanhtoan1705@users.noreply.github.com>
2024-07-20 09:29:18 +0700

    [ThanhToan] - update

---
+++ QuanLyNhaHang/database/seeders/DishSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Dish;
+use GuzzleHttp\Promise\Create;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DishSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Dish::create([
+            'name' => 'Lẫu thái',
+            'category_id' => '1',
+            'description' => 'bét seo lơ',
+            'price' => '10000000',
+            'image' => '123.png'
+        ]);
+    }
+}
